75d52dd52dbcb294e2da81a382391f3e68dd697f
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2018-12-30 13:41:25 +0700

    Update info.js

--- commands/zycos/info.js
+++ commands/zycos/info.js
@@ -19,19 +19,19 @@ class infoCommand extends commando.Command {
     async run(message)
     {
       message.channel.send({embed: new Discord.RichEmbed()  
-        .setTitle("**Zycos II Information**")
+        .setTitle("**Vymic II Information**")
         .setColor("#FF0000")
         .addField("**__Server Information__**", "----------------------------------")
-        .addField("**• Server Name**", "Zycos Network")
+        .addField("**• Server Name**", "Vymic Network")
         .addField("**• Server Owner**", "Zymaz")
         .addField("**• Server Host**", "Thailand & Singapore")
         .addField("**• Minigame**", "Practice/KitMap/HCF/UHC")
         .addField("**__Links__**", "----------------------------------")
-        .addField("**Server IP**", "ZYCOS.CF")
+        .addField("**Server IP**", "vymicpvp.cf")
         .addField("**Website**", "Comming soon")
         .addField("**Store**", "Comming soon")
         .addField("**Discord**", "https://discord.gg/RF8uy5K")
-        .setFooter("Zycos bot created by hieu#0843")
+        .setFooter("Vymic bot created by hieu#0843")
         .setTimestamp()})
     }
 }
